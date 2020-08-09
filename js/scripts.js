eb76dd80b021bc2a03d45b119d149a31a0bb8f26
Jeremy Neumann <jneumann@unitedstatesappraisals.com>
2020-08-09 14:03:01 -0500

    add copyright script

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+
+// Update Copyright entry to display current year. //Credit @DevinFord
+document
+    .getElementById('currentYear')
+    .appendChild(document.createTextNode(new Date().getFullYear()));
